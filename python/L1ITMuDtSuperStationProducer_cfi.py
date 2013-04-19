c9711b5c2b369605c8eb011621968afba6101d18
indianajam <giuseppe.codispoti@cern.ch>
2013-04-19 12:51:40 +0200

    Added rpc-to-dt matching inside DtSuperStation. A list of rpc hits closest than a given MaxDeltaPhi is associated to each DT primitive. The list is sorted from the closest to the farthest

--- python/L1ITMuDtSuperStationProducer_cfi.py
+++ python/L1ITMuDtSuperStationProducer_cfi.py
@@ -2,5 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 L1ITMuDtSuperStationProducer = cms.EDProducer(
     'L1ITMuDtSuperStationProducer',
-    TriggerPrimitiveSrc = cms.InputTag('L1ITMuTriggerPrimitives')
+    TriggerPrimitiveSrc = cms.InputTag('L1ITMuTriggerPrimitives'),
+    MaxDeltaPhi = cms.double( 2 )
     )
