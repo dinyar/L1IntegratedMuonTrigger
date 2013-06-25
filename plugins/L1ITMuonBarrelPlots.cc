fe52a176812ba275abb0d66600e6f6cf86f69094
jacopo <pazzini@pd.infn.it>
2013-06-25 19:57:52 +0200

    minor changes

--- plugins/L1ITMuonBarrelPlots.cc
+++ plugins/L1ITMuonBarrelPlots.cc
@@ -14,7 +14,6 @@
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
-#include "FWCore/Framework/interface/ESHandle.h"
 
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
