1d2a36b18dcffa252554eb586f09a1c23bcccbca
Carlo Battilana <carlo.battilana@gmail.com>
2013-07-29 12:30:53 +0200

    First version of skimmed production (GEN-SIM-L1) for Crab submission

---
+++ test/particleGunGenSimCrab.py
@@ -0,0 +1,127 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.381.2.7 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: SingleMuFlatLogPt_100MeVto2TeV_cfi.py -s GEN,SIM,DIGI,L1 --conditions START53_V7A::All --eventcontent FEVTDEBUG --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('L1')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.GeometrySimDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic8TeVCollision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.Digi_cff')
+process.load('Configuration.StandardSequences.SimL1Emulator_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(300)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.381.2.7 $'),
+    annotation = cms.untracked.string('SingleMuFlatPt_5GeVto200GeV_cfi.py nevts:500'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+
+# Output definition
+
+process.FEVTDEBUGoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.FEVTDEBUGEventContent.outputCommands,
+    fileName = cms.untracked.string('/data2/battilan/L1Trigger/BarrelDTTFUpgrade/SingleMuFlatPt_3GeVto140GeV_GEN_SIM_DIGI_L1_CrabJ.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'START53_V7A::All', '')
+
+process.generator = cms.EDProducer("FlatRandomPtGunProducer",
+	PGunParameters = cms.PSet(
+        MinPt = cms.double(3),
+	MaxPt = cms.double(140),
+        PartID = cms.vint32(-13),        
+        MaxPhi = cms.double(3.14159265359/6.),
+	MinPhi = cms.double(-3.14159265359/6.),
+	MaxEta = cms.double(0.85),
+        MinEta = cms.double(-0.85)        
+    ),
+    Verbosity = cms.untracked.int32(0),
+    psethack = cms.string('single mu pt 5to100'),
+    AddAntiParticle = cms.bool(False), #need *single* muons dammit
+    firstRun = cms.untracked.uint32(1)
+)
+
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.digitisation_step = cms.Path(process.pdigi)
+process.L1simulation_step = cms.Path(process.SimL1Emulator)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.FEVTDEBUGoutput_step = cms.EndPath(process.FEVTDEBUGoutput)
+
+#outCommands = cms.untracked.vstring('drop *')
+#outCommands.append('keep *_simMuonDTDigis_*_*')
+#outCommands.append('keep *_simMuonRPCDigis_*_*')
+#outCommands.append('keep *_simMuonCSCDigis_*_*')
+#outCommands.append('keep *_genParticles_*_*')
+#outCommands.append('keep *_simCsctfTrackDigis_*_*')
+#outCommands.append('keep *_simDttfDigis_*_*')
+#outCommands.append('keep *_simGmtDigis_*_*')
+#outCommands.append('keep *_simRpcTriggerDigis_*_*')
+#outCommands.append('keep *_simDtTriggerPrimitiveDigis_*_*')
+#outCommands.append('keep *_simCscTriggerPrimitiveDigis_*_*')
+#outCommands.append('keep *_L1ITMuTriggerPrimitives_*_*')
+#outCommands.append('keep *_MBLTProducer_*_*')
+#outCommands.append('keep *_MBTracksProducer_*_*')
+#outCommands.append('keep *_L1ITMuonBarrelPrimitiveProducer_*_*')
+#outCommands.append('keep *_*Converter_*_*')
+#outCommands.append('keep *_*Matcher_*_*')
+
+#process.FEVTDEBUGoutput.outputCommands = outCommands
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,
+				process.genfiltersummary_step,
+				process.simulation_step,
+				process.digitisation_step,
+				process.L1simulation_step,
+				process.endjob_step,
+				process.FEVTDEBUGoutput_step)
+
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.generator * getattr(process,path)._seq 
+
