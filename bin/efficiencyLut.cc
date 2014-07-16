35917f818b011ef84e36cc9920f1b6370421a21c
gian luca pinna angioni <gian.luca.pinna.angioni@cern.ch>
2014-07-16 13:50:13 +0200

    now LUT are created using lowedge bin instead of bin center

--- bin/efficiencyLut.cc
+++ bin/efficiencyLut.cc
@@ -77,7 +77,7 @@ void efficiency::write2( TH2F * plot, std::ofstream & outfile, int inCh, int out
 	  --y;
 	}
 	break;
-      }
+      } 
       effPrev = eff;
     }
     // std::cout << binWidth << '\t' << binStart << '\t' << xBins << '\t' << binStart + binWidth*x  << '\t' << xSum << std::endl;
@@ -96,7 +96,7 @@ void efficiency::write( TH2F * plot, std::ofstream & outfile, int inCh, int outC
 			const TString & ref1, const TString & ref2 )
 {
 
-  TAxis * xAxis = plot->GetXaxis();
+  // TAxis * xAxis = plot->GetXaxis();
   TAxis * yAxis = plot->GetYaxis();
 
   size_t xBins = plot->GetNbinsX();
@@ -132,8 +132,8 @@ void efficiency::write( TH2F * plot, std::ofstream & outfile, int inCh, int outC
     testfile_ << plot->GetName() << '\t' << plot->GetBinCenter( x ) << '\t' << eff << std::endl; 
 
     outfile << inCh << '\t' << outCh << '\t' << ref1 << '\t' << ref2 << '\t'
-	    << xAxis->GetBinCenter( x ) << '\t' << yAxis->GetBinCenter( y ) << '\t' << eff << std::endl;
-
+      // << xAxis->GetBinCenter( x ) << '\t' << yAxis->GetBinCenter( y ) << '\t' << eff << std::endl; // use bin center instead of bin lowedge
+	    <<plot->GetBinLowEdge(x) << '\t' << yAxis->GetBinCenter( y ) << '\t' << eff << std::endl; 
   }
 
 
@@ -163,9 +163,11 @@ void efficiency::run( bool byTh )
 	    for ( size_t ch1 = 0; ch1 < 2; ++ch1 ) {
 	      for ( size_t ch2 = 0; ch2 < 2; ++ch2 ) {
 
-		TString name = Form( "L1ITMuPtPlotter/Wh%dSc%d/hd%svsPtWh%dSc%dinCh%doutCh%d%s%s",
-				     wheels[w], sector, params[p].Data(), wheels[w], sector, inCh[ch1], outCh[ch2], ref[rf1].Data(), ref[rf2].Data() );
+	
 
+		TString name = Form( "L1ITMuPtPlotter/Wh%dSc0/hd%svsPtWh%dSc0inCh%doutCh%d%s%s",
+				     wheels[w], params[p].Data(), wheels[w], inCh[ch1], outCh[ch2], ref[rf1].Data(), ref[rf2].Data() );  //so far use sector 0 for every sectors
+		
 		//if(rf1==1) std::cout<<" name "<<name.Data()<<std::endl;
 		TH2F * plot = (TH2F *)(file_->Get( name ));
 
